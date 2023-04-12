36d546cd071925e9f66b1a269e148ff99e395e5a
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-04-12 11:25:49 +0530

    Create Trapping Rainwater.cpp

---
+++ Trapping Rainwater.cpp
@@ -0,0 +1,68 @@
+Trapping Rainwater
+
+Problem Statement: Given an array of non-negative integers representation elevation of ground. Your task is to find the water that can be trapped after rain.
+
+
+
+Solution 1: Brute force
+Approach: For each index, we have to find the amount of water that can be stored and we have to sum it up.If we observe carefully the amount the water stored at a particular index is the minimum of maximum elevation to the left and right of the index minus the elevation at that index.
+
+
+
+int trap(vector<int>& height) {
+        int n=height.size() , sum=0;
+        
+        if(n==1)
+            return 0;
+        
+        int left[n] , right[n];
+        left[0] = height[0] , right[n-1] = height[n-1];
+        for(int i=1 ; i<n ; i++){
+            left[i] = max(left[i-1] , height[i]);
+        } 
+        for(int i=n-2 ; i>=0 ; i--){
+            right[i] = max(height[i] , right[i+1]);
+        }
+        
+        for(int i=0 ; i<n ; i++){
+            int x = min(left[i] , right[i]);
+            sum += x-height[i];
+        }
+        
+        return sum;
+    }
+
+
+
+
+
+Solution 2:Better solution
+Intuition: We are taking O(N) for computing leftMax and rightMax at each index. The complexity can be boiled down to O(1) if we precompute the leftMax and rightMax at each index.
+Approach: Take 2 array prefix and suffix array and precompute the leftMax and rightMax for each index beforehand. Then use the formula min(prefix[I], suffix[i])-arr[i] to compute water trapped at each index.
+
+int trap(vector<int>& height) {
+        int n = height.size();
+        int leftmax = 0, rightmax = 0;
+        int res = 0;
+        int l=0, r=n-1;
+        
+        while(l<=r) {
+            if(height[l] <= height[r]) {
+                if(height[l] > leftmax) {
+                    leftmax = height[l];
+                } else {
+                    res += leftmax - height[l];
+                }
+                l++;
+            } else {
+                if(height[r] > rightmax) {
+                    rightmax = height[r];
+                } else {
+                    res += rightmax - height[r];
+                }
+                r--;
+            }
+        }
+        
+        return res;
+    }
