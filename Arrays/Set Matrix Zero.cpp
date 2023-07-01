028573a1603974c4ed3e0338dbe85702b4ca4f02
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-01 14:49:19 +0530

    Update Set Matrix Zero.cpp

--- Arrays/Set Matrix Zero.cpp
+++ Arrays/Set Matrix Zero.cpp
@@ -78,3 +78,48 @@ public:
 Space optimization
 
 We can use the first row and first column of the matrix as map has been used in the previous solution ..
+
+
+
+	void setZeros(vector<vector<int>> &matrix)
+{
+	int r = matrix.size();
+	int c = matrix[0].size();
+	// HERE WE ARE USING THE FIRST ROW AND FIRST COLUMN TO KEEP TRACK OF 0's
+	// instead of that extra space we used in the previous approach
+	int col0 = -1;
+	for(int i=0 ; i<r ; i++) {
+		for(int j=0 ; j<c ; j++) {
+			if(matrix[i][j] == 0) {
+				matrix[i][0] = 0;
+				if(j!=0) {
+					matrix[0][j] = 0;
+				} else {
+					col0 = 0;
+				}
+			}
+		}
+	}
+
+	for(int i=1 ; i<r ; i++) {
+		for(int j=1 ; j<c ; j++) {
+			if(matrix[i][j]!=0 && (matrix[i][0] == 0 || matrix[0][j] == 0)) {
+				matrix[i][j] = 0;
+			}
+		}
+	}
+
+	if(matrix[0][0] == 0) {
+		// for the row
+		for(int i=0 ; i<c ; i++) {
+			matrix[0][i] = 0;
+		}
+	}
+
+	if(col0 == 0) {
+		for(int i=0 ; i<r ; i++) {
+			matrix[i][0] = 0;
+		}
+	}
+	
+}
