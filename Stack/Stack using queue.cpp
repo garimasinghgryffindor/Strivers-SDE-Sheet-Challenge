e470ad040335ca382e3d830b31c4bd15da3717f0
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-19 14:24:19 +0530

    Update Stack using queue.cpp

--- Stack/Stack using queue.cpp
+++ Stack/Stack using queue.cpp
@@ -1,9 +1,5 @@
-// first two are by using two queues
 
-// HERE I MADE THE PUSH OPERATION COSTLY O(N)
-// AND POP O(N)
-
-// SC -> O(N)
+PUSH OPERATION COSTLY
 class Stack {
 
     queue<int>q1,q2;
@@ -25,7 +21,7 @@ class Stack {
         return false;
     }
 
-    void push(int element) { // O(N)
+    void push(int element) {
         q2.push(element);
         while(!q1.empty()) {
             q2.push(q1.front());
@@ -35,7 +31,7 @@ class Stack {
         swap(q1,q2);
     }
 
-    int pop() { // O(1)
+    int pop() {
         if(!size) {
             return -1;
         }
@@ -52,7 +48,6 @@ class Stack {
         return q1.front();
     }
 };
-
 
 
 // POP OPERATION COSTLY
@@ -113,5 +108,59 @@ class Stack {
         }
         swap(q1, q2);
         return x;
+    }
+};
+
+
+
+// using 1 QUEUE
+// push operation => O(N)
+class Stack {
+
+    queue<int>q;
+    int size;
+   public:
+    Stack() {
+        size = 0;
+    }
+
+    /*----------------- Public Functions of Stack -----------------*/
+
+    int getSize() {
+        return size;
+    }
+
+    bool isEmpty() {
+        if(size == 0)
+            return true;
+        return false;
+    }
+
+    void push(int element) {
+        q.push(element);
+        
+        for(int i=0 ; i<size ; i++) {
+            int front = q.front();
+            q.pop();
+            q.push(front);
+        }
+        size++;
+    }
+
+    int pop() {
+        if(!size) {
+            return -1;
+        }
+        int x = q.front();
+        q.pop();
+        size--;
+        return x;
+    }
+
+    int top() {
+        if(!size) {
+            return -1;
+        }
+        return q.front();
     }
 };
