90ed957aa7d7ff76d4688e42762fe33a6e8212c8
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-19 14:09:05 +0530

    Update Stack using queue.cpp

--- Stack/Stack using queue.cpp
+++ Stack/Stack using queue.cpp
@@ -1,3 +1,5 @@
+// first two are by using two queues
+
 // HERE I MADE THE PUSH OPERATION COSTLY O(N)
 // AND POP O(N)
 
@@ -48,5 +50,68 @@ class Stack {
             return -1;
         }
         return q1.front();
+    }
+};
+
+
+
+// POP OPERATION COSTLY
+
+class Stack {
+
+    queue<int>q1,q2;
+    int size;
+   public:
+    Stack() {
+        size = 0;
+    }
+
+    /*----------------- Public Functions of Stack -----------------*/
+
+    int getSize() {
+        return size;
+    }
+
+    bool isEmpty() {
+        if(size == 0)
+            return true;
+        return false;
+    }
+
+    void push(int element) {
+        q1.push(element);
+        size++;
+    }
+
+    int pop() {
+        if(!size) {
+            return -1;
+        }
+        int x;
+        while(!q1.empty()) {
+            x = q1.front();
+            q1.pop();
+            if(q1.empty()) {
+                continue;
+            }
+            q2.push(x);
+        }
+        size--;
+        swap(q1, q2);
+        return x;
+    }
+
+    int top() {
+        if(!size) {
+            return -1;
+        }
+        int x;
+        while(!q1.empty()) {
+            x = q1.front();
+            q1.pop();
+            q2.push(x);
+        }
+        swap(q1, q2);
+        return x;
     }
 };
