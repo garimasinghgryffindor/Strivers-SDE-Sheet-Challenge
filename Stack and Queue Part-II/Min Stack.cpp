2d29c8a072573b4510193f4224f0844cb8f698a1
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-20 21:09:51 +0530

    Update Min Stack.cpp

--- Stack and Queue Part-II/Min Stack.cpp
+++ Stack and Queue Part-II/Min Stack.cpp
@@ -48,3 +48,72 @@ class minStack
 			return stk.top().second;
 		}
 };
+
+
+
+#include <bits/stdc++.h> 
+// Implement class for minStack.
+// using MATHEMATICS AND STACK
+// SC-> O(1)
+class minStack
+{
+	public:
+		stack<long>stk;
+		long minm;
+		minStack() { }
+		
+		void push(int num)
+		{
+			if(stk.empty()) {
+				stk.push(num);
+				minm = num;
+			} else {
+				if(num <= minm) {
+					// change the minm
+					int prevMin = minm;
+					minm = num;
+					stk.push(2ll*minm - prevMin);
+				} else {
+					// simply push
+					stk.push(num);
+				}
+			}
+		}
+		
+		int pop()
+		{
+			if(stk.empty()) return -1;
+			
+			if(stk.top() <= minm) {
+				// current element is the minimum
+				// actual current element is
+				int tp = minm;
+				minm = 2*minm - stk.top();
+				stk.pop();
+				return tp;
+			} else {
+				int tp = stk.top();
+				stk.pop();
+				return tp;
+			}
+		}
+		
+		int top()
+		{
+			if(stk.empty()) return -1;
+			
+			if(stk.top() <= minm) {
+				// current element is the minimum
+				// actual current element is
+				return minm;
+			} else {
+				return stk.top();
+			}
+		}
+		
+		int getMin()
+		{
+			if(stk.empty()) return -1;
+			return minm;
+		}
+};
