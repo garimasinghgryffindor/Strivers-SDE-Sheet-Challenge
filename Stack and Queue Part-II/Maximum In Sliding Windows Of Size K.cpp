15b3e9a4c63220231265f1645a81c043f5250c3f
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-20 20:16:23 +0530

    Update Maximum In Sliding Windows Of Size K.cpp

--- Stack and Queue Part-II/Maximum In Sliding Windows Of Size K.cpp
+++ Stack and Queue Part-II/Maximum In Sliding Windows Of Size K.cpp
@@ -35,3 +35,38 @@ vector<int> slidingWindowMaximum(vector<int> &nums, int &k)
 
     return res;
 }
+
+
+
+// OPTIMIZED
+// using monotonic deque
+// monotonically decreasing
+// so that front is always max
+vector<int> slidingWindowMaximum(vector<int> &nums, int &k)
+{
+    vector<int>res;
+    deque<int>dq;
+    int n = nums.size();
+
+    for(int i=0 ; i<n ; i++) {
+        if(!dq.empty() && dq.front() == i-k) {
+            dq.pop_front();
+        }
+
+        while(!dq.empty() && nums[dq.back()] < nums[i]) {
+            dq.pop_back();
+        }
+
+        dq.push_back(i);
+
+        if(i>=k-1) {
+            res.push_back(nums[dq.front()]);
+        }
+    }
+
+    return res;
+}
+
+
+
+
