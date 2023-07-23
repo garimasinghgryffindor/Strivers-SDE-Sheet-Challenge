729e14f529111eee71f0baeb5b7bbb4bdb5ec1c9
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-23 19:23:42 +0530

    Update Longest Palindromic Substring.cpp

--- String/Longest Palindromic Substring.cpp
+++ String/Longest Palindromic Substring.cpp
@@ -24,3 +24,38 @@ string longestPalinSubstring(string str) {
 
     return longest;
 }
+
+
+// DP
+string longestPalinSubstring(string str) {
+    int n = str.length();
+    bool dp[n][n];
+    int maxLength = 0, start = 0;
+    memset(dp, 0, sizeof(dp));
+    for(int i=0 ; i<n ; i++) {
+        dp[i][i] = true;
+        maxLength = 1;
+    }
+    for(int i=0 ; i<n-1 ; i++) {
+        if(str[i]==str[i+1]) {
+            dp[i][i+1] = true;
+            start = i;
+            maxLength = 2;
+        }
+    }
+
+    for(int i=3 ; i<=n ; i++) {
+        for(int j=0 ; j<n-i+1 ; j++) {
+            int k = i+j-1;
+            if(dp[j+1][k-1] && str[j]==str[k]) {
+                dp[j][k] = true;
+                if(maxLength < i) {
+                    start = j;
+                    maxLength = i;
+                }
+            }
+        }
+    }
+
+    return str.substr(start,maxLength);
+}
