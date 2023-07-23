8d0f9a7c0fb96a5fb3cdbba294ab1383bfa6012d
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-23 18:23:16 +0530

    Update Reverse Words In A String.cpp

--- String/Reverse Words In A String.cpp
+++ String/Reverse Words In A String.cpp
@@ -25,3 +25,30 @@ string reverseString(string &str){
 
 	return ans;
 }
+
+
+
+// using vector
+string reverseString(string &str){
+	vector<string>vec;
+	string ans = "";
+	int n = str.length();
+	for(int i=0 ; i<n ; i++) {
+		string temp = "";
+		while(!isspace(str[i]) && i<n) {
+			temp.push_back(str[i++]);
+		}
+		if(!temp.empty()) {
+			vec.push_back(temp);
+		}
+	}
+
+	for(int i=vec.size()-1 ; i>=0 ; i--) {
+		ans.append(vec[i]);
+		if(i>0) {
+			ans.append(" ");
+		}
+	}
+
+	return ans;
+}
