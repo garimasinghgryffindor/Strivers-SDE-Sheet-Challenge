7d13483f83dedd4d61438657df8c2cf6c600baf5
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-27 17:19:48 +0530

    Update Check Permutation.cpp

--- String Part-II/Check Permutation.cpp
+++ String Part-II/Check Permutation.cpp
@@ -5,3 +5,23 @@ bool areAnagram(string &str1, string &str2){
     sort(str2.begin(), str2.end());
     return str1 == str2;
 }
+
+
+// using hash map
+bool areAnagram(string &str1, string &str2){
+    vector<int>mp(26,0), mp2(26,0);
+    for(auto x: str1) {
+        mp[x-'a']++;
+    }
+
+    for(auto x: str2) {
+        mp2[x-'a']++;
+    }
+
+    for(int i=0 ; i<26 ; i++) {
+        if(mp[i] != mp2[i])
+            return false;
+    }
+
+    return true;
+}
