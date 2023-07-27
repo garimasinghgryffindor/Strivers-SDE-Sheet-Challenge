2561eb6a096b6b7d03f4cca4e166c246ac36e4b6
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-27 17:11:44 +0530

    Create Check Permutation.cpp

---
+++ String Part-II/Check Permutation.cpp
@@ -0,0 +1,7 @@
+// using sorting
+// TC: O(NlogN)
+bool areAnagram(string &str1, string &str2){
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+    return str1 == str2;
+}
