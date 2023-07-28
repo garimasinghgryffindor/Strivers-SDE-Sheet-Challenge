d1e3dded67796049fbc9d95fabef7e0e88a935b3
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-28 14:49:40 +0530

    Update Right View of Binary Tree.cpp

--- Binary Tree/Right View of Binary Tree.cpp
+++ Binary Tree/Right View of Binary Tree.cpp
@@ -17,3 +17,38 @@ public:
         return res;
     }
 };
+
+
+// using LEVEL ORDER TRAVERSAL 
+// BFS
+class Solution {
+public:
+    void bfs(TreeNode*node, vector<int>&res) {
+        if(!node)
+            return;
+        
+        queue<TreeNode*>q;
+        q.push(node);
+        while(!q.empty()) {
+            int tp = q.front()->val;
+            res.push_back(tp);
+            int n = q.size();
+            for(int i=0 ; i<n ; i++) {
+                TreeNode *ptr = q.front();
+                if(ptr->right) {
+                    q.push(ptr->right);
+                }
+                if(ptr->left) {
+                    q.push(ptr->left);
+                }
+                q.pop();
+            }
+        }
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>res;
+        bfs(root, res);
+        return res;
+    }
+};
