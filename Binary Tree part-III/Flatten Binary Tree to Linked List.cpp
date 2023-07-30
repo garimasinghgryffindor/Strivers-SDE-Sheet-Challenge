a7a9fa11286d306727aa6dbc8d45205bf6261224
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-30 16:33:42 +0530

    Update Flatten Binary Tree to Linked List.cpp

--- Binary Tree part-III/Flatten Binary Tree to Linked List.cpp
+++ Binary Tree part-III/Flatten Binary Tree to Linked List.cpp
@@ -26,3 +26,29 @@ TreeNode<int> *flattenBinaryTree(TreeNode<int> *root)
     preorder(root,pre);
     return createLinkedList(pre);
 }
+
+
+
+// APPROACH 2
+// WITHOUT USING EXTRA SPACE
+// IN-PLACE
+TreeNode<int> *flattenBinaryTree(TreeNode<int> *root)
+{
+    if(!root) return root;
+    
+    TreeNode<int>* curr = root;
+    while(curr) {
+        if(curr->left) {
+            TreeNode<int>*pred = curr->left;
+            while(pred->right) {
+                pred = pred->right;
+            }
+            pred->right = curr->right;
+            curr->right = curr->left;
+            curr->left = NULL;
+        }
+        curr = curr->right;
+    }
+
+    return root;
+}
