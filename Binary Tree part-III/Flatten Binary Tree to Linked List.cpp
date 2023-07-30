77c4efc82cffd47d714e90eceeb38a2a11317e98
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-30 16:06:55 +0530

    Create Flatten Binary Tree to Linked List.cpp

---
+++ Binary Tree part-III/Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,28 @@
+// APPROACH 1 -> NAIVE APPROACH
+// SAVE THE PREORDER OF THE TREE IN THE VECTOR
+// AND CREATE A LINKED LIST USING THIS VECTOR
+void preorder(TreeNode<int>*root, vector<int>&res) {
+    if(!root) return ;
+    res.push_back(root->data);
+    preorder(root->left, res);
+    preorder(root->right, res);
+}
+
+TreeNode<int>* createLinkedList(vector<int>res) {
+    TreeNode<int>*dummy = new TreeNode<int>(0);
+    TreeNode<int>* curr = dummy;
+    
+    for(int i=0 ; i<res.size() ; i++) {
+        TreeNode<int>* node = new TreeNode<int>(res[i]);
+        curr->right = node;
+        curr = curr->right;
+    }
+    return dummy->right;
+}
+
+TreeNode<int> *flattenBinaryTree(TreeNode<int> *root)
+{
+    vector<int>pre;
+    preorder(root,pre);
+    return createLinkedList(pre);
+}
