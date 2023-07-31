10655af484b7f5d0183fcd00ff13d8ccd4be76eb
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-31 16:00:39 +0530

    Update K-th largest Number BST.cpp

--- Binary Search Tree Part-II/K-th largest Number BST.cpp
+++ Binary Search Tree Part-II/K-th largest Number BST.cpp
@@ -18,3 +18,30 @@ int KthLargestNumber(TreeNode<int>* root, int k)
     }
     return inorder[n-k];
 }
+
+
+
+// using inorder traversal
+// TC: O(N)
+// SC: O(1)
+bool inOrder(TreeNode<int>*node, int&k, int&res) {
+    if(!node) return false;
+    
+    bool r1 = inOrder(node->right,k,res);
+    if(r1) return true;
+
+    k--;
+    if(k == 0) {
+        res = node->data;
+        return true;
+    }
+
+    bool l1 = inOrder(node->left,k,res);
+    return l1;
+}
+int KthLargestNumber(TreeNode<int>* root, int k) 
+{
+    int res = -1;
+    bool ans = inOrder(root,k,res);
+    return res;
+}
