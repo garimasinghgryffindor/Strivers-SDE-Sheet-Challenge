b6018f823d357d14d97b07805007ad630f6e3248
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-31 15:11:37 +0530

    Create Floor in a BST.cpp

---
+++ Binary Search Tree Part-II/Floor in a BST.cpp
@@ -0,0 +1,20 @@
+// BRUTE FORCE APPROACH 
+// COMPARING WITH ALL THE NODES
+void recur(TreeNode<int>*root, int x, int&res) {
+    if(!root) return;
+    if(root->val <= x) {
+        res = max(res,root->val);
+        recur(root->right,x,res);
+        return;
+    }
+    recur(root->left,x,res);
+}
+
+int floorInBST(TreeNode<int> * root, int X)
+{
+    int res = INT_MIN;
+    recur(root,X,res);
+    return res;
+}
+
+
