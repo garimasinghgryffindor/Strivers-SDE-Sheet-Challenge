0dfc30e876ba31149c4bd8b43611b4e658c705d7
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-31 15:32:48 +0530

    Update Floor in BST.cpp

--- Binary Search Tree Part-II/Floor in BST.cpp
+++ Binary Search Tree Part-II/Floor in BST.cpp
@@ -1,18 +1,38 @@
-// BRUTE FORCE APPROACH 
-// COMPARING WITH ALL THE NODES
+// RECURSIVELY
 void recur(TreeNode<int>*root, int x, int&res) {
     if(!root) return;
     if(root->val <= x) {
-        res = max(res,root->val);
+        res = root->val;
         recur(root->right,x,res);
         return;
     }
     recur(root->left,x,res);
 }
-
 int floorInBST(TreeNode<int> * root, int X)
 {
-    int res = INT_MIN;
+    int res = -1;
     recur(root,X,res);
+    return res;
+}
+
+
+// ITERATIVELY
+int floorInBST(TreeNode<int> * root, int x)
+{
+    int res = -1;
+    TreeNode<int>*node = root;
+    while(node) {
+        if(node->val == x) {
+            res = x;
+            return res;
+        }
+        if(node->val < x) {
+            res = node->val;
+            node = node->right;
+        } else {
+            node = node->left;
+        }
+    }
+
     return res;
 }
