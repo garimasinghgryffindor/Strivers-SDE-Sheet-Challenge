352b4725e13a7f55fbe19bea7d747862ea82a567
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-05 20:44:39 +0530

    Update Longest Increasing Subsequence.cpp

--- Dynamic Programming/Longest Increasing Subsequence.cpp
+++ Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -38,3 +38,24 @@ int lengthOfLIS(vector<int>& nums) {
     return lis(nums, 0, -1);
 }
 
+
+
+// using TABULATION
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<int>dp(n, 0);
+    dp[0] = 1;
+    for(int i = 1 ; i < n ; i++) {
+        dp[i] = 1;
+        for(int j = 0 ; j < i ; j++) {
+            if(nums[j] < nums[i] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+    
+    return *max_element(dp.begin(), dp.end());
+}
+
+
+
