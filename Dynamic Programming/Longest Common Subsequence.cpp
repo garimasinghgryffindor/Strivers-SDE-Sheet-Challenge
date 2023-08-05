9ef9bfb3df2e4df523a06b97269a3acfb18c8e9b
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-05 20:58:23 +0530

    Update Longest Common Subsequence.cpp

--- Dynamic Programming/Longest Common Subsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence.cpp
@@ -23,3 +23,31 @@ int longestCommonSubsequence(string text1, string text2) {
 
 
 
+// using MEMOIZATION
+int n, m;
+vector<vector<int>>dp;
+int recur(string&s1, string&s2 , int i, int j) {
+    if(i>=n || j>=m) return 0;
+        
+    if(dp[i][j] != -1) return dp[i][j];
+    
+    int take = 0;
+    if(s1[i] == s2[j]) {
+        take = 1 + recur(s1, s2, i+1 , j+1);
+    }
+    
+    int notTake1 = 0;
+    int notTake2 = 0;
+    notTake1 = 0 + recur(s1, s2, i+1, j);
+    notTake2 = 0 + recur(s1, s2, i, j+1);
+    
+    return dp[i][j] = max({take, notTake1, notTake2});
+}
+
+int longestCommonSubsequence(string text1, string text2) {
+    n = text1.length(), m = text2.length();
+    dp.resize(n, vector<int>(m, -1));
+    return recur(text1, text2, 0, 0);
+}
+
+
