d3864a8f3f424f8b05bfccae8ef14e30935fad5c
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-05 19:46:37 +0530

    Update Maximum Product Subarray.cpp

--- Dynamic Programming/Maximum Product Subarray.cpp
+++ Dynamic Programming/Maximum Product Subarray.cpp
@@ -16,4 +16,36 @@ int maximumProduct(vector<int> &arr, int n)
 }
 
 
+// APPROACH 2
+// using 2 Traversals
+int maximumProduct(vector<int> &arr, int n)
+{
+	int maxLeft = arr[0], maxRight = arr[0];
+	int prod = 1;
+	bool zeroPresent = false;
+	for(int x: arr) {
+		prod *= x;
+		if(x == 0) {
+			prod = 1;
+			zeroPresent = true;
+			continue;
+		}
+		maxLeft = max(maxLeft, prod);
+	}
+
+	prod = 1;
+	for(int i=n-1 ; i>=0 ; i--) {
+		prod *= arr[i];
+		if(arr[i] == 0) {
+			prod = 1;
+			zeroPresent = true;
+			continue;
+		}
+		maxRight = max(maxRight, prod);
+	}
+
+	if(zeroPresent) return max(max(maxRight, maxLeft), 0);
+	return max(maxRight, maxLeft);
+}
+
 
