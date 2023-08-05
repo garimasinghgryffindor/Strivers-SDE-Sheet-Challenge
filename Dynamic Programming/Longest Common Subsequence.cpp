bd2b65bac84ade88d5983ed20ae73ddd6c6ca7c3
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-05 21:08:51 +0530

    Update Longest Common Subsequence.cpp

--- Dynamic Programming/Longest Common Subsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence.cpp
@@ -51,3 +51,33 @@ int longestCommonSubsequence(string text1, string text2) {
 }
 
 
+
+// using TABULATION
+int longestCommonSubsequence(string text1, string text2) {
+    int n = text1.length(), m = text2.length();
+    vector<vector<int>>dp;
+    dp.resize(n+1, vector<int>(m+1, -1));
+    
+    for(int i=0 ; i<=n ; i++) {
+        dp[i][0] = 0;
+    }
+    
+    for(int i=0 ; i<=m ; i++) {
+        dp[0][i] = 0;
+    }
+    
+    for(int i=1 ; i<=n ; i++) {
+        for(int j=1 ; j<=m ; j++) {
+            if(text1[i-1] == text2[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else {
+                dp[i][j] = 0 + max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+    }
+    
+    return dp[n][m];
+}
+
+
