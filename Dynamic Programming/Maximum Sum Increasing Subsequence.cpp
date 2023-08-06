ef8943951de78ca0568735a196f3e2df6718931c
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-06 19:44:05 +0530

    Update Maximum Sum Increasing Subsequence.cpp

--- Dynamic Programming/Maximum Sum Increasing Subsequence.cpp
+++ Dynamic Programming/Maximum Sum Increasing Subsequence.cpp
@@ -17,3 +17,26 @@ int maxIncreasingDumbbellsSum(vector<int> &rack, int n)
 {
 	return recur(rack, 0, 0);
 }
+
+
+// MEMOIZATION
+int recur(vector<int>&rack, int idx, int prevWeight,vector<vector<int>>&dp) {
+	if(idx >= rack.size()) return 0;
+
+	if(dp[idx][prevWeight+1] != -1) return dp[idx][prevWeight+1];
+
+	int take = 0, notTake = 0;
+	if(prevWeight == -1 || rack[idx] > rack[prevWeight]) {
+		take = rack[idx] + recur(rack, idx+1, idx, dp);
+	}
+
+	notTake = 0 + recur(rack, idx+1, prevWeight, dp);
+
+	return (dp[idx][prevWeight+1] = max(take, notTake));
+}
+
+int maxIncreasingDumbbellsSum(vector<int> &rack, int n)
+{
+	vector<vector<int>>dp(n, vector<int>(n+1, -1));
+	return recur(rack, 0, -1,dp);
+}
