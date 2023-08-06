136f87536db376e1b1d5953f6d43239815cfe5b4
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-06 14:08:17 +0530

    Update Edit Distance.cpp

--- Dynamic Programming/Edit Distance.cpp
+++ Dynamic Programming/Edit Distance.cpp
@@ -87,3 +87,32 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    // TABULATION
+    int minDistance(string word1, string word2) {
+        int n = word1.length(), m = word2.length();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        
+        for(int i = 0 ; i<=n ; i++) {
+            dp[i][0] = i;
+        }
+        
+        for(int j=0 ; j<=m ; j++) {
+            dp[0][j] = j;
+        }
+        
+        for(int i=1 ; i<=n ; i++) {
+            for(int j=1 ; j<=m ; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1+min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
