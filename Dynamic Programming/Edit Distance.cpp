229f620ffb10c46dc2374473f4b1a48abcad25b6
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-06 13:59:44 +0530

    Update Edit Distance.cpp

--- Dynamic Programming/Edit Distance.cpp
+++ Dynamic Programming/Edit Distance.cpp
@@ -24,3 +24,34 @@ public:
         return recur(word1, word2, 0, 0);
     }
 };
+
+
+// other way
+class Solution {
+public:
+    // RECURSION
+    int n,m;
+    
+    int recur(string&s1, string&s2, int i, int j) {
+        if(i < 0) {
+            return (j+1);
+        }
+        if(j < 0) {
+            return (i+1);
+        }
+        
+        if(s1[i] == s2[j]) {
+            return 0 + recur(s1, s2, i-1, j-1);
+        }
+        
+        return 1+min({recur(s1,s2,i-1,j-1), recur(s1,s2,i-1,j), recur(s1,s2,i,j-1) });
+    }
+    
+    int minDistance(string word1, string word2) {
+        n = word1.length(), m = word2.length();
+        
+        return recur(word1, word2, n-1, m-1);
+    }
+};
+
+
