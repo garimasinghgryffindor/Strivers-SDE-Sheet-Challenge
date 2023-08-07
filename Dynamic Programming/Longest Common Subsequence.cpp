b36cbcaa9d185010c2a4bb97da45444f7bf450a2
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-07 14:47:26 +0530

    Update Longest Common Subsequence.cpp

--- Dynamic Programming/Longest Common Subsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence.cpp
@@ -81,3 +81,84 @@ int longestCommonSubsequence(string text1, string text2) {
 }
 
 
+
+// TRY 2 (DIY)
+
+// // using recursion
+// int recur(string&s, string&t, int i, int j) {
+// 	if(i < 0 || j < 0) return 0;
+
+// 	if(s[i] == t[j]) {
+// 		return 1 + recur(s,t,i-1,j-1);
+// 	}
+// 	int left = 0 + recur(s,t,i-1,j);
+// 	int right = 0 + recur(s,t,i,j-1);
+// 	return max(left, right);
+// }
+
+// int lcs(string s, string t)
+// {
+// 	int n = s.length(), m = t.length();
+// 	return recur(s,t,n-1,m-1);
+// }
+
+
+// // using MEMOIZATION
+// #include <bits/stdc++.h>
+// int recur(string&s, string&t, int i, int j,vector<vector<int>>&dp) {
+// 	if(i < 0 || j < 0) return 0;
+
+// 	if(dp[i][j] != -1) return dp[i][j];
+
+// 	if(s[i] == t[j]) {
+// 		return 1 + recur(s,t,i-1,j-1,dp);
+// 	}
+// 	int left = 0 + recur(s,t,i-1,j,dp);
+// 	int right = 0 + recur(s,t,i,j-1,dp);
+// 	return dp[i][j] = max(left, right);
+// }
+
+// int lcs(string s, string t)
+// {
+// 	int n = s.length(), m = t.length();
+// 	vector<vector<int>>dp(n, vector<int>(m,-1));
+// 	return recur(s,t,n-1,m-1,dp);
+// }
+
+
+// using TABULATION
+#include <bits/stdc++.h>
+// int recur(string&s, string&t, int i, int j,vector<vector<int>>&dp) {
+// 	if(i < 0 || j < 0) return 0;
+
+// 	if(dp[i][j] != -1) return dp[i][j];
+
+// 	if(s[i] == t[j]) {
+// 		return 1 + recur(s,t,i-1,j-1,dp);
+// 	}
+// 	int left = 0 + recur(s,t,i-1,j,dp);
+// 	int right = 0 + recur(s,t,i,j-1,dp);
+// 	return dp[i][j] = max(left, right);
+// }
+
+int lcs(string s, string t)
+{
+	int n = s.length(), m = t.length();
+	vector<vector<int>>dp(n+1, vector<int>(m+1,0));
+
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m; j++) {
+			if(s[i] == t[j]) {
+				dp[i+1][j+1] = 1 + dp[i][j];
+				continue;
+			}
+			int left = 0 + dp[i][j+1];
+			int right = 0 + dp[i+1][j];
+			dp[i+1][j+1] = max(left, right);
+		}
+ 	}
+
+	return dp[n][m];
+}
+
+
