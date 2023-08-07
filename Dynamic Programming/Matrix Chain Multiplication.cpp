99f87be0ce6c3de2f2df67fb10e1d8a4e253e599
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-07 13:10:03 +0530

    Update Matrix Chain Multiplication.cpp

--- Dynamic Programming/Matrix Chain Multiplication.cpp
+++ Dynamic Programming/Matrix Chain Multiplication.cpp
@@ -10,8 +10,30 @@ int recur(vector<int>&arr, int i, int j) {
 
     return res;
 }
-
 int matrixMultiplication(vector<int> &arr, int N)
 {
     return recur(arr,1,N-1);
+}
+
+
+
+// USING MEMOIZATION
+int recur(vector<int>&arr, int i, int j, vector<vector<int>>&dp) {
+    if(i == j) return 0;
+
+    if(dp[i][j] != -1) return dp[i][j];
+
+    int res = INT_MAX;
+    for(int k = i ; k < j ; k++) {
+        int operations = arr[i-1]*arr[k]*arr[j];
+        int total_operations = operations + recur(arr,i,k,dp) + recur(arr,k+1,j,dp);
+        res = min(res, total_operations);
+    }
+
+    return dp[i][j]=res;
+}
+int matrixMultiplication(vector<int> &arr, int N)
+{
+    vector<vector<int>>dp(N,vector<int>(N,-1));
+    return recur(arr,1,N-1,dp);
 }
