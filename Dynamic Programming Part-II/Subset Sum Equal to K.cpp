90907407dfb8cd8f1a0b27de4664d69d4ca84ca6
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-07 21:32:32 +0530

    Update Subset Sum Equal to K.cpp

--- Dynamic Programming Part-II/Subset Sum Equal to K.cpp
+++ Dynamic Programming Part-II/Subset Sum Equal to K.cpp
@@ -14,3 +14,25 @@ bool recur(vector<int>&arr, int sum, int idx) {
 bool subsetSumToK(int n, int k, vector<int> &arr) {
     return recur(arr, k, n-1);
 }
+
+
+// using MEMOIZATION
+bool recur(vector<int>&arr,int sum,int idx,vector<vector<int>>&dp) {
+    if(sum == 0) return true;
+    if(idx < 0) return false;
+
+    if(dp[idx][sum] != -1) return dp[idx][sum];
+
+    bool take = 0;
+    if(arr[idx] <= sum) {
+        take = recur(arr, sum - arr[idx], idx-1, dp);
+    }
+    bool notTake = recur(arr, sum, idx-1, dp);
+
+    return dp[idx][sum] = take || notTake;
+}
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<int>>dp(n, vector<int>(k+1, -1));
+    return recur(arr, k, n-1,dp);
+}
+
