f96e9dca64aa119be4627a29d3327e33ce803870
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-08 08:51:06 +0530

    Update Rod cutting problem.cpp

--- Dynamic Programming Part-II/Rod cutting problem.cpp
+++ Dynamic Programming Part-II/Rod cutting problem.cpp
@@ -39,3 +39,23 @@ int cutRod(vector<int> &price, int n)
 	return recur(price, n, 1, 0, dp);
 }
 
+
+// USING TABULATION
+int cutRod(vector<int> &price, int n)
+{
+	vector<vector<int>>dp(n+2, vector<int>(n+1,0));
+
+	for(int idx = n; idx >= 0; idx--) {
+		for(int prevCut = idx-1; prevCut >=0; prevCut--) {
+			int cutLength = idx-prevCut;
+			int cutValue = price[cutLength-1] + dp[idx+1][idx];
+
+			// or not
+			int cutValue2 = dp[idx+1][prevCut];
+			dp[idx][prevCut] = max(cutValue, cutValue2);
+		}
+	}
+
+	return dp[1][0];
+}
+
