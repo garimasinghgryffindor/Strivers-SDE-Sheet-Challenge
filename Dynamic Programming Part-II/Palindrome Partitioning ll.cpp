1669c52ffe263f276b97efbb0e2f046912afa3b1
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-08 16:58:57 +0530

    Update Palindrome Partitioning ll.cpp

--- Dynamic Programming Part-II/Palindrome Partitioning ll.cpp
+++ Dynamic Programming Part-II/Palindrome Partitioning ll.cpp
@@ -5,7 +5,6 @@ bool chkPalindrome(string s) {
     }
     return true;
 }
-
 int recur(string str, int idx, int n) {
     if(idx == n) return 0;
 
@@ -19,9 +18,41 @@ int recur(string str, int idx, int n) {
     }
     return mn;
 }
-
 int palindromePartitioning(string str)
 {
     int n = str.length();
     return recur(str, 0, n) - 1;
 }
+
+
+// using MEMOIZATION
+bool chkPalindrome(string s) {
+    for(int i=0 ; i<s.length()/2 ; i++) {
+        if(s[i] != s[s.length() - i -1]) return false;
+    }
+    return true;
+}
+int recur(string str, int idx, int n, vector<int>&dp) {
+    if(idx == n) return 0;
+
+    if(dp[idx] != -1) return dp[idx];
+
+    int mn = INT_MAX;
+    for(int k = idx; k < n; k++) {
+        string curr = str.substr(idx, k-idx+1);
+        if(chkPalindrome(curr)) {
+            int cuts = 1 + recur(str, k+1, n, dp);
+            mn = min(cuts, mn);
+        }
+    }
+    return dp[idx] = mn;
+}
+int palindromePartitioning(string str)
+{
+    int n = str.length();
+    vector<int>dp(n, -1);
+    return recur(str, 0, n, dp) - 1;
+}
+
+
+
