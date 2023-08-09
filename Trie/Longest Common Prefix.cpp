f576dc4337177f0d9d078cfd7d7cc3d84f534271
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-09 11:28:46 +0530

    Update Longest Common Prefix.cpp

--- Trie/Longest Common Prefix.cpp
+++ Trie/Longest Common Prefix.cpp
@@ -36,7 +36,6 @@ class Trie {
         return idx;
     }
 };
-
 string longestCommonPrefix(vector<string> &arr, int n)
 {
     Trie trie;
@@ -48,5 +47,24 @@ string longestCommonPrefix(vector<string> &arr, int n)
 
     return arr[0].substr(0, LCPLength);
 }
+
+
+// USING SORTING
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    sort(arr.begin(), arr.end());
+    int l = arr[0].length();
+    string ans = "";
+    for(int i=0 ; i<l ; i++) {
+        if(arr[0][i] == arr[n-1][i]) {
+            ans.push_back(arr[0][i]);
+        } else {
+            return ans;
+        }
+    }
+    return ans;
+}
+
+
 
 
