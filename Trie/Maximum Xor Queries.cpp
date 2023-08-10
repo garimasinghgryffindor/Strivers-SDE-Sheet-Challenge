0a125a4ffdf2f99c6c9d03f8523a9f599b18eaff
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-10 20:50:12 +0530

    Update Maximum Xor Queries.cpp

--- Trie/Maximum Xor Queries.cpp
+++ Trie/Maximum Xor Queries.cpp
@@ -59,3 +59,77 @@ public:
         return res;
     }
 };
+
+
+
+// APPROACH 3
+// OPTIMAL TRIE AND BIT MANIPULATION USING SORTING AND OFFLINE QUERIES
+// APPLYING THE TRIE AND BIT MANIPULATION SOLUTION for OFFLINE QUERIES
+// i.e.; we will be creating only 1 trie;
+// and sort the queries in increasing order as per the mi
+struct Node {
+    Node *links[2];
+};
+
+class Solution {
+public:
+    Node * root;
+    
+    void insert(int num) {
+        Node * node = root;
+        for(int i = 31; i >= 0; i--) {
+            int LMbit = (num >> i) & 1;
+            if(!node->links[LMbit]) {
+                node->links[LMbit] = new Node();
+            }
+            node = node->links[LMbit];
+        }
+    }
+    
+    int maxXor(int num) {
+        int res = 0;
+        Node * node = root;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if(node->links[!bit]) {
+                res = res | (1 << i);
+                node = node->links[!bit];
+            } else {
+                node = node->links[bit];
+            }
+        }
+        return res;
+    }
+
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        int q = queries.size();
+        vector<int>res(q,0);
+        root = new Node();
+        vector<vector<int>>vec;
+        for(int i = 0; i < q; i++) {
+            vec.push_back({queries[i][1], queries[i][0], i});
+        }
+        
+        sort(vec.begin(), vec.end());
+        sort(nums.begin(), nums.end());
+        
+        bool flag = false;
+        int idx = 0;
+        for(int i = 0; i < q; i++) {
+            int val = vec[i][1];
+            int thresh = vec[i][0];
+            int actualIndx = vec[i][2];
+            for(idx; idx < nums.size() && nums[idx] <= thresh ; idx++) {
+                if(nums[idx] <= thresh) {
+                    insert(nums[idx]);
+                    flag = true;
+                }
+            }
+            if(!flag) { res[actualIndx] = -1; continue; }
+            int ans = maxXor(val);
+            res[actualIndx] = ans;
+        }
+        
+        return res;
+    }
+};
