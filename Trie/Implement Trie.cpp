e2f2c0c3887b5b881c6971aa76f0ae976e469b32
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-29 20:16:49 +0530

    Update Implement Trie.cpp

--- Trie/Implement Trie.cpp
+++ Trie/Implement Trie.cpp
@@ -44,3 +44,66 @@ public:
         return true;
     }
 };
+
+
+
+// using TRIE and DFS
+struct Node {
+    Node * links [26];  
+    bool flag = false;
+};
+class Solution {
+public:
+    Node * root;
+    
+    void insert(string word) {
+        int n = word.length();
+        Node * node = root;
+        for(int i = 0; i < n; i++) {
+            if(!node->links[word[i]-'a']) {
+                node->links[word[i]-'a'] = new Node();
+            }
+            node = node->links[word[i]-'a'];
+        }
+        node->flag = true;
+    }
+    
+    bool dfs(string word,Node * node, int index, int count) {
+        if(index >= word.length()) { return count > 1; }
+        
+        int n = word.length();
+        Node * curr = node;
+        
+        for(int i = index; i < n; i++) {
+            if(!curr->links[word[i] - 'a']) { return false; }
+            curr = curr->links[word[i]-'a'];
+            if(curr->flag) {
+                if(dfs(word,node,i+1,count+1)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        vector<string> res;
+        // using Trie
+        root = new Node();
+        
+        for(auto s: words) {
+            insert(s);
+        }
+        
+        for(int i = 0; i < words.size(); i++) {
+            bool ans = dfs(words[i], root, 0, 0);
+            if(ans) res.push_back(words[i]);
+        }
+        
+        return res;
+    }
+};
+
+
+
