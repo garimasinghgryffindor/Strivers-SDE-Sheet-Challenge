cf5cf81824b3b3068692cb9112bb186d687963fb
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-02-12 19:16:30 +0530

    Update The Celebrity Problem.cpp

--- Stack and Queue Part-II/The Celebrity Problem.cpp
+++ Stack and Queue Part-II/The Celebrity Problem.cpp
@@ -59,3 +59,33 @@ int findCelebrity(int n) {
 
 	return potentialPerson;
 }
+
+
+// it's almost same
+int findCelebrity(int n) {
+ 	stack<int>st;
+	for(int i = 0; i < n; i++) st.push(i);
+
+	while(st.size() > 1) {
+		int a = st.top(); 	st.pop();
+		int b = st.top(); 	st.pop();
+		if(!knows(a,b)) {
+			st.push(a);
+		} else if(!knows(b, a)){
+			st.push(b);
+		}
+	}
+
+	if(st.empty()) return -1;
+
+	int potentialCeleb = st.top();
+
+	for(int i = 0; i < n; i++) {
+		if(i != potentialCeleb) {
+			if(!knows(i, potentialCeleb)) return -1;
+			if(knows(potentialCeleb, i)) return -1;
+		}
+	}
+
+	return potentialCeleb;
+}
